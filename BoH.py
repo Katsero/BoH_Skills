64c89279ee80bd9c6d09f397154207467701482c
Katsero <raleksandrd25@gmail.com>
2025-07-19 00:14:35 +0300

    Update BoH.py

--- BoH.py
+++ BoH.py
@@ -2,8 +2,8 @@
 Made By Katsero. Sorry for Russian in code, too lazy to translate, let's say you have a translator, right? My apologies
 
 Any way, here is a code that helps to get all wanted soul parts from tree (If you have same ADHD and perfectionism trait).
-First list is made if trees and amount of slots you're going to use. Prefereble NOT to touch it, here is my minimal distribution tree (all branches in order from upper one - Illumination.
-High use of warps and skips, yet you can any way fill not used slots after the count
+First list is made if trees and amount of slots you're going to use. Prefereble NOT to touch it, here is my minimal distribution tree (all branches in order from upper one - Illumination).
+High use of warps and skips to get all wisdoms to level 9, yet you can still fill unused slots in process.
 
 Optimised restribution
 0 1 1 0 0 0 1 1 1 
@@ -21,7 +21,7 @@ Keep in mind that you only have 73 skills. If summary of soul parts is more you'
 
 Next one is list of skills. All skills | first tree | soul part from first tree | sec tree | sec SP
 
-You can change ammount of attempts on line
+You can change amount of attempts on line
 'distribution = distribute_skills(tries=1000000)'
 If you really need perfect result for ALL 73 skills, lol
 
